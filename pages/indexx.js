3119e3bb8d907d7e63fe0e1aa2ba81435bd48795
Muneshkumar12 <munesh788mk@gmail.com>
2023-08-25 12:54:31 +0500

    form

---
+++ pages/indexx.js
@@ -0,0 +1,12 @@
+
+function addData(){
+    console.log("Name--------->", document.getElementById("name").value)
+    console.log("Father's Name-------->" , document.getElementById("fatherName").value)
+    console.log("Email--------->" , document.getElementById("email").value)
+    console.log("Password---------->" , document.getElementById("password").value)
+    console.log("CNIC Number-------->" , document.getElementById("cnic").value)
+    console.log("Mobile--------->" , document.getElementById("Mobile").value)
+    console.log("Course--------->" , document.getElementById("course").value)
+    console.log("Qualificatio--------->" , document.getElementById("qualification").value)
+    console.log("Gender--------->" , document.getElementById("gender").value)
+}
