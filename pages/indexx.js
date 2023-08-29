aa2cf845245099c28117fd28fc4ed6be31eb2c25
Muneshkumar12 <munesh788mk@gmail.com>
2023-08-29 15:14:43 +0500

    new update

--- pages/indexx.js
+++ pages/indexx.js
@@ -9,52 +9,97 @@
    var Gender = document.getElementById("gender");
 function submitData(){
     if(Name.value === ""){
-        message.style.display = "block"
+        message.style.display = "block";
+        message.innerHTML = "please enter your name";
+        message.style.color = "red";
+        Name.focus();
+        setTimeout(() => {
+            message.style.display = "none";
+        }, 1000);
+    }else if(Fathername.value === ""){
+        message.style.display = "block";
+        message.innerHTML = "Please enter your father's name";
+        message.style.color = "red";
+        Fathername.focus();
         setTimeout(() => {
             message.style.display = "none"
         }, 1000);
-    }else if(Fathername.value === ""){
-        message2.style.display = "block"
-        setTimeout(() => {
-            message2.style.display = "none"
-        }, 1000);
     }else if(Email.value === ""){
-        message3.style.display = "block"
+        message.style.display = "block"
+        message.innerHTML = "Please enter your email address";
+        message.style.color = "red";
+        Email.focus();
         setTimeout(() => {
-            message3.style.display = "none"
+            message.style.display = "none"
         }, 1000);
     }else if(Password.value === ""){
-        message4.style.display = "block"
+        message.style.display = "block"
+        message.innerHTML = "Please enter your password";
+        message.style.color = "red";
+        Password.focus();
         setTimeout(() => {
-            message4.style.display = "none"
+            message.style.display = "none"
         }, 1000);
     }else if(cnicnum.value === ""){
-        message5.style.display = "block"
+        message.style.display = "block"
+        message.innerHTML = "Please enter your CNIC Number";
+        message.style.color = "red";
+        cnicnum.focus();
         setTimeout(() => {
-            message5.style.display = "none"
+            message.style.display = "none"
         }, 1000);
-    }else if(Mobile.value === ""){
-        message6.style.display = "block"
+    }else if(cnicnum.value.length !== 14){
+        message.style.display = "block";
+        message.innerHTML = "please enter valid CNIC number"
+        message.style.color = "red";
+        cnicnum.focus();
         setTimeout(() => {
-            message6.style.display = "none"
+            message.style.display = "none"
         }, 1000);
-    }else if(Course.value === ""){
-        message7.style.display = "block"
+    } else if(Mobile.value === ""){
+        message.style.display = "block"
+        message.innerHTML = "Please enter your Mobile number";
+        message.style.color = "red";
+        Mobile.focus();
         setTimeout(() => {
-            message7.style.display = "none"
+            message.style.display = "none"
+        }, 1000);
+    }else if(Mobile.value.length !== 11){
+        message.style.display = "block";
+        message.innerHTML = "please enter valid phone number"
+        message.style.color = "red";
+        Mobile.focus();
+        setTimeout(() => {
+            message.style.display = "none"
+        }, 1000);
+    }  else if(Course.value === ""){
+        message.style.display = "block"
+        message.innerHTML = "Please select your course";
+        message.style.color = "red";
+        Course.focus();
+        setTimeout(() => {
+            message.style.display = "none"
         }, 1000);
     }else if(Qualification.value === ""){
-        message8.style.display = "block"
+        message.style.display = "block"
+        message.innerHTML = "Please select your Qualification";
+        message.style.color = "red";
+        Qualification.focus();
         setTimeout(() => {
-            message8.style.display = "none"
+            message.style.display = "none"
         }, 1000);
     }else if(Gender.value === ""){
-        message9.style.display = "block"
+        message.style.display = "block"
+        message.innerHTML = "Please Select Your Gender";
+        message.style.color = "red";
+        Gender.focus();
         setTimeout(() => {
-            message9.style.display = "none"
+            message.style.display = "none"
         }, 1000);
     }else{
-        success.style.display = "block"
+        message.style.display = "block";
+        message.innerHTML = "Your form has been submit successfuly";
+        message.style.color = "green";
         console.log("Name)" , Name.value)
         console.log("Father's name)" , Fathername.value)
         console.log("Email)" , Email.value)
@@ -65,16 +110,16 @@ function submitData(){
         console.log("Qualification)" , Qualification.value)
         console.log("Gender)" , Gender.value)
         setTimeout(() => {
-            success.style.display = "none"
+           message.style.display = "none";
+           Name.value = ""
+           Fathername.value = ""
+           Email.value = ""
+           Password.value = ""
+           cnicnum.value = ""
+           Mobile.value = ""
+           Course.value = ""
+           Qualification.value = ""
+           Gender.value = ""
         }, 2000);
-        Name.value = ""
-        Fathername.value = ""
-        Email.value = ""
-        Password.value = ""
-        cnicnum.value = ""
-        Mobile.value = ""
-        Course.value = ""
-        Qualification.value = ""
-        Gender.value = ""
     }
 }
